629ce4c3b55f3456c7a31e409e9bdcb2b8736293
Kraigo <kraigo@live.com>
2016-04-24 12:32:30 +0300

    Twitter client oauth

--- tocotter.js
+++ tocotter.js
@@ -1,27 +1,40 @@
-var Twitter = require('twitter');
+'use strict';
+
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var request = require('request');
-var cheerio = require('cheerio')
+var cheerio = require('cheerio');
+
+var twitterAPI  = require('node-twitter-api');
+var config = require('./config.js');
+var twitter = new twitterAPI(config.twitterAccess);
+
+var Datastore = require('nedb');
+var db = new Datastore({ filename: 'authorization.db', autoload: true });
+db.ensureIndex({ fieldName: 'expirationDate', expireAfterSeconds: 0 }, function(err) {
+	if (err) throw err;
+});
 
 var multiparty = require('connect-multiparty');
 var multipartyMiddleware = multiparty();
 
-var config = require('./config');
-
-var open = require('open');
-
 var app = express();
 
 app.use(bodyParser());
+app.use(cookieParser());
 app.use(express.static('public'));
 
-// app.use(busboy());
 
 
-var client = new Twitter(config.twitterAccess);
+// var client = new Twitter(config.twitterAccess);
+// var client = {};
 
 app.get('/api/:section/:action', function (req, res) {
+
+	db.findOne({accessToken: req.cookies.accessToken}, function(err, auth) {
+
+	});
 	client.get('/'+req.params.section+'/'+req.params.action, req.query, function(error, tweets, response){
 		if (!error) {
 			res.send(tweets);
@@ -29,29 +42,29 @@ app.get('/api/:section/:action', function (req, res) {
 	});	
 });
 
-app.post('/api/:section/:action', multipartyMiddleware, function (req, res) {
-	// console.log('/'+req.params.section+'/'+req.params.action, req.body);
+// app.post('/api/:section/:action', multipartyMiddleware, function (req, res) {
+// 	// console.log('/'+req.params.section+'/'+req.params.action, req.body);
 
-	//if (req.body.file) {
-    //
-	//	console.log(req.body.file);
-	//	client.post('/media/upload', {media_data: req.body.file}, function(error, media, response){
-	//		console.log(error);
-	//		if (!error) {
-	//			console.log(media.media_id_strin);
-	//			res.send(media.media_id_string);
-	//		}
-	//	});
-	//} else {
-		client.post('/'+req.params.section+'/'+req.params.action, req.body, function(error, tweets, response){
-			if (!error) {
-				res.send(tweets);
-			}
-		});
-	//}
+// 	//if (req.body.file) {
+//     //
+// 	//	console.log(req.body.file);
+// 	//	client.post('/media/upload', {media_data: req.body.file}, function(error, media, response){
+// 	//		console.log(error);
+// 	//		if (!error) {
+// 	//			console.log(media.media_id_strin);
+// 	//			res.send(media.media_id_string);
+// 	//		}
+// 	//	});
+// 	//} else {
+// 		client.post('/'+req.params.section+'/'+req.params.action, req.body, function(error, tweets, response){
+// 			if (!error) {
+// 				res.send(tweets);
+// 			}
+// 		});
+// 	//}
 
 
-});
+// });
 
 //app.get('/request/:link', function(req, res) {
 //	var options = {
@@ -78,7 +91,95 @@ app.post('/api/:section/:action', multipartyMiddleware, function (req, res) {
 //	})
 //});
 
+
+app.get('/auth', function (req, res) {
+
+	twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
+
+		if (error) return console.log("Error getting OAuth request token : " + error);
+		
+		var expires= new Date();		
+		expires.setHours(expires.getHours() + 1);		
+
+		res.cookie('requestToken', requestToken, {expires: expires, httpOnly: false, domain: req.hostname});
+
+		db.insert({
+			requestToken: requestToken,
+			requestTokenSecret: requestTokenSecret,
+			expirationDate: expires
+		}, function(err, doc) {
+			if (err) console.log(err);
+
+			res.redirect(twitter.getAuthUrl(requestToken));
+		});
+
+		
+
+	})
+
+});
+
+app.get('/callback', function (req, res) {
+
+	db.findOne({requestToken: req.cookies.requestToken}, function(err, auth) {
+
+		if (err) return console.log(err);
+
+		twitter.getAccessToken(auth.requestToken, auth.requestTokenSecret, req.query.oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
+		
+			if (error) return console.log(error);
+
+			var expires = new Date();		
+			expires.setFullYear(expires.getFullYear() + 10);		
+
+			res.cookie('accessToken', accessToken, {expires: expires, httpOnly: false, domain: req.hostname});
+
+			var dbupdate = {
+				$set: {
+					accessToken: accessToken,
+					accessTokenSecret: accessTokenSecret
+				},
+				$unset: {
+					expirationDate: true
+				}
+			};
+
+			db.update({ requestToken: req.cookies.requestToken }, dbupdate, {},
+				function () {
+					res.redirect('/timeline');
+				}
+			);
+			
+
+		})
+
+	});
+	
+});
+
+app.get('/timeline', function (req, res) {
+
+	db.findOne({accessToken: req.cookies.accessToken}, function(err, auth) {
+
+		var options = {};
+		twitter.getTimeline("home_timeline", 
+			options,
+			auth.accessToken,
+			auth.accessTokenSecret,
+			function(error, data, response) {
+				if (error) return console.log(error);
+
+				res.send(data);
+			}
+		);
+
+	});
+
+	
+});
+
 var server = app.listen(8080, function () {
 	console.log('Tocotter listening :8080');
-	open('http://localhost:8080');
+	// open('http://localhost:8080');
 });
+
