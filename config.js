b77b1f5c62aa8e73b7626194c40d2a1cb8770368
Kraigo <kraigo@live.com>
2016-04-24 12:29:41 +0300

    Config

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 var twitterAcess = {
-    consumer_key: 'CONSUMER_KEY',
-    consumer_secret: 'CONSUMER_SECRET',
-    access_token_key: 'ACCESS_TOKEN_KEY',
-    access_token_secret: 'ACCESS_TOKEN_SECRET'
+    // From your app
+
+    consumerKey: process.env.CONSUMER_KEY || 'CONSUMER KEY',
+    consumerSecret: process.env.CONSUMER_SECRET || 'CONSUMER SECRET KEY',
+    callback: 'http://localhost:8080/callback'
 };
 
 module.exports = {
-    twitterAcess: twitterAcess
+    twitterAccess: twitterAcess
 };
