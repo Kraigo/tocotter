fa8b0e49352510b43179d1b0bfa5f65510407881
Kraigo <kraigo@live.com>
2016-05-12 01:20:12 +0300

    WS https

--- public/app/services/stream.js
+++ public/app/services/stream.js
@@ -1,7 +1,8 @@
 "use strict";
 
 app.service('stream', function(ngSocket){
-    var stream = ngSocket('ws://' + window.location.host);
+    var protocol = location.protocol.indexOf('https') >= 0 ? 'wss' : 'ws';
+    var stream = ngSocket(protocol + '://' + window.location.host);
     stream.onOpen(function() {
         console.log('Stream User opened');
     });
